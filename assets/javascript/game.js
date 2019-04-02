c438d2929b47a9fb03891a4dbf21aea272a1038b
Brad Cas <bmcasalvieri@gmail.com>
2019-04-01 21:09:21 -0400

    fixed up game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,13 +2,18 @@
 $(document).ready(function() {
 
   // Create variables
-  var targetNumber;
+  var randomNumber;
   var totalScore;
   var wins = 0;
   var losses = 0;
   var numberOptions = [];
   var gameRunning = false;
-  var $targetNumber
+  var $randomNumber = $("#randomNumber");
+  var $totalScore = $("#totalScore");
+  var $wins = $("#wins");
+  var $losses = $("#losses");
+  var $crystals = $("#crystals");
+  var $crystalImage = $(".crystalImage");
 
   // NEW GAME
   function newGame() {
@@ -17,8 +22,8 @@ $(document).ready(function() {
     gameRunning = true;
     
     // Select a random target number and write to page
-    targetNumber = 1 + (Math.floor(Math.random() * 100));
-    $("#targetNumber").text(targetNumber);
+    randomNumber = 1 + (Math.floor(Math.random() * 100));
+    $randomNumber.text(randomNumber);
   
     // Reset numberOptions array
     numberOptions = [];
@@ -32,40 +37,45 @@ $(document).ready(function() {
     // Assign crystal values
     for (var i = 0; i < numberOptions.length; i++) {
       var imageCrystal = $("<img>");
-      imageCrystal.addClass("crystal-image");
+      imageCrystal.addClass("crystalImage, img-fluid, col-3");
       imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
       imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-      $("#crystals").append(imageCrystal);
+      $crystals.append(imageCrystal);
     };
   
     // Reset total score
     totalScore = 0;
-    $("#totalScore").text(totalScore);
+    $totalScore.text(totalScore);
   
   };
   
   // GAME PLAY
   // User clicks on a crystal
-  $(".crystal-image").on("click", function() {
+  $crystalImage.on("click", function() {
+
+    if (!gameRunning) {
+      alert(`Click button to start a new game!`);
+      return false;
+    };
   
     var crystalValue = ($(this).attr("data-crystalvalue"));
     crystalValue = parseInt(crystalValue);
   
     // Add points to total score, and write to page
     totalScore += crystalValue;
-    $("#totalScore").text(totalScore);
+    $totalScore.text(totalScore);
   
     // WIN-LOSE LOGIC
     // If total score === target score, user wins; add 1 to wins and write to page
     if (totalScore === targetNumber) {
       wins++;
-      $("#wins").text(wins);
+      $wins.text(wins);
       gameRunning = false;
     }
     // If total score > target score, user loses; add 1 to losses and write to page
     else if (totalScore > targetNumber) {
       losses++;
-      $("#losses").text(losses);
+      $losses.text(losses);
       gameRunning = false;
     };
     
